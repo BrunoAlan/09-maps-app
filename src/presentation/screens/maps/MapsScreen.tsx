f011c8ffbbe799e8992c52b30b3e0e9341fabd03
BrunoAlan <alan.brunoab@gmail.com>
2024-07-31 12:32:01 -0300

    initialize project

---
+++ src/presentation/screens/maps/MapsScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const MapsScreen = () => {
+  return (
+    <View>
+      <Text>MapsScreen</Text>
+    </View>
+  );
+};
+export default MapsScreen;
---
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+const PermissionsScreen = () => {
+  return (
+    <View>
+      <Text>PermissionsScreen</Text>
+    </View>
+  );
+};
+export default PermissionsScreen;
