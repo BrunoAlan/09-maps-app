4fb91443720d60d459a11c3ebd5afca8bbec59aa
BrunoAlan <alan.brunoab@gmail.com>
2024-08-01 10:16:40 -0300

    feat: Add watchLocation and clearWatchLocation functions for tracking user location

--- src/presentation/store/location/useLocationStore.ts
+++ src/presentation/store/location/useLocationStore.ts
@@ -1,16 +1,38 @@
-import { getCurrentLocation } from '@/src/actions/location/location';
+import {
+  clearWatchLocation,
+  getCurrentLocation,
+  watchCurrentLocation,
+} from '@/src/actions/location/location';
 import type { Location } from '@/src/infrastructure/interfaces/location';
+import { LocationSubscription } from 'expo-location/build/Location.types';
 import { create } from 'zustand';
 interface LocationState {
   lastKnowLocation: Location | null;
+  userLocations: Location[];
+  locationSuscription?: LocationSubscription | null;
   getLocation: () => Promise<Location>;
+  watchLocation: () => Promise<void>;
+  clearWatchLocation: () => void;
 }
 
 export const useLocationStore = create<LocationState>()((set, get) => ({
   lastKnowLocation: null,
+  userLocations: [],
+  locationSuscription: null,
   getLocation: async () => {
     const location = await getCurrentLocation();
     set({ lastKnowLocation: location });
     return location;
+  },
+  watchLocation: async () => {
+    const locationSuscription = await watchCurrentLocation((location) => {
+      set({ lastKnowLocation: location });
+      set((state) => ({ userLocations: [...state.userLocations, location] }));
+    });
+    set({ locationSuscription });
+  },
+  clearWatchLocation: () => {
+    const { locationSuscription } = get();
+    clearWatchLocation(locationSuscription!);
   },
 }));
