695f81aceb6d8b2de52a53c8d855462f28b50985
BrunoAlan <alan.brunoab@gmail.com>
2024-07-31 23:27:58 -0300

    feat: Update PermissionsChecker to handle location permissions and navigation

--- src/presentation/providers/PermissionsChecker.tsx
+++ src/presentation/providers/PermissionsChecker.tsx
@@ -1,6 +1,36 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
+import { AppState } from 'react-native';
+import { usePermissionStore } from '../store/permissions/usePermissionStore';
+import { useRouter } from 'expo-router';
 
 const PermissionsChecker = ({ children }: PropsWithChildren) => {
+  const { locationStatus, checkLocationPermission } = usePermissionStore();
+  const router = useRouter();
+
+  console.log(locationStatus);
+  useEffect(() => {
+    if (locationStatus === 'granted') {
+      router.replace('/maps');
+    } else if (locationStatus === 'undetermined') {
+      router.navigate('/permission');
+    }
+  }, [locationStatus]);
+
+  useEffect(() => {
+    checkLocationPermission();
+  }, [checkLocationPermission]);
+
+  useEffect(() => {
+    const susbcription = AppState.addEventListener('change', (nextAppState) => {
+      if (nextAppState === 'active') {
+        checkLocationPermission();
+      }
+    });
+    return () => {
+      susbcription.remove();
+    };
+  }, [checkLocationPermission]);
+
   return <>{children}</>;
 };
 export default PermissionsChecker;
